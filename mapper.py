7987d9b2bb97cf3717ad84d2585157eb7ec04267
Panagiotis Doupidis <72436005+androidDev18026@users.noreply.github.com>
2021-04-01 15:35:20 +0300

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -4,4 +4,5 @@ from sys import stdin as input_stream
 
 for line in input_stream:
     ip, date, time = line.strip().split(',')[:3]
+    # output <date,time> as a value
     print ("%s,%s,%s" %(ip,date,time))
