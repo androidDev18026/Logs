0b6a3fbec32097d9aba055d6c04428db17872c05
Panagiotis Doupidis <72436005+androidDev18026@users.noreply.github.com>
2021-04-01 15:32:05 +0300

    fix output to emit <ip , <date,time>> pairs

--- mapper.py
+++ mapper.py
@@ -3,5 +3,5 @@
 from sys import stdin as input_stream
 
 for line in input_stream:
-    ip, _, time = line.strip().split(',')[:3]
-    print ("%s,%s" %(ip,time))
+    ip, date, time = line.strip().split(',')[:3]
+    print ("%s,%s,%s" %(ip,date,time))
