30c5ebd36b4771610690dd560db2ee6cd27fd561
Panagiotis Doupidis <72436005+androidDev18026@users.noreply.github.com>
2021-04-09 17:06:26 +0300

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -5,6 +5,6 @@ from sys import stdin as input_stream
 for line in input_stream:
     ip, date, time = line.strip().split(',')[:3]
 
-		# check if line starts with ip
+    # check if line starts with ip
     if ip[0].isdigit():
         print ("%s,%s,%s" %(ip,date,time))
