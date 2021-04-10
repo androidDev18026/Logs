7b8497f17bb428cddfb69670803272abc21ad2e8
Panagiotis Doupidis <72436005+androidDev18026@users.noreply.github.com>
2021-04-10 11:59:33 +0300

    filter input

--- mapper.py
+++ mapper.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
+import re
 from sys import stdin as input_stream
+
+valid_ip = re.compile(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\w{1,3}$")
+valid_date = re.compile(r"\d{4}-\d{2}-\d{2}$")
+valid_time = re.compile(r"\d{2}:\d{2}:\d{2}$")
 
 for line in input_stream:
     ip, date, time = line.strip().split(',')[:3]
 
-    # check if line starts with ip
-    if ip[0].isdigit():
+	# check if line starts with ip
+    if valid_ip.match(ip) and valid_date.match(date) and valid_time.match(time):
         print ("%s,%s,%s" %(ip,date,time))
