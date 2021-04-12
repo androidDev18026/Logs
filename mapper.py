e6670fd736b99a8d4f678d887635addfb040de01
Panagiotis Doupidis <72436005+androidDev18026@users.noreply.github.com>
2021-04-12 13:04:38 +0300

    Delete mapper.py

--- mapper.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-from sys import stdin as input_stream
-
-for line in input_stream:
-    ip, date, time = line.strip().split(',')[:3]
-    # output <date,time> as a value
-    print ("%s,%s,%s" %(ip,date,time))
